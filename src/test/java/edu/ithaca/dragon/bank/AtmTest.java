8bd082d9f2ecc7f5dad8c21afa0976a339548e57
Sean Blackford <57670508+SeanBlackford37@users.noreply.github.com>
2021-02-18 10:01:19 -0500

    Added ATM tests to ATM test file

---
+++ src/test/java/edu/ithaca/dragon/bank/AtmTest.java
@@ -0,0 +1,48 @@
+package edu.ithaca.dragon.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AtmTest {
+
+    @Test
+    void getBalanceTest() {
+        BankAccount bankAccount = new BankAccount("a@b.com", 200);
+        ATM atmOne = new ATM();
+        assertEquals(200, atmOne.checkBalance(bankAccount)); //Correct Balance displays 
+        BankAccount bankAccountTwo = new BankAccount("a@b.com", 10.50);  //Balance with a Double
+        assertEquals(10.50, atmOne.checkBalance(bankAccountTwo));
+        BankAccount bankAccountThree = new BankAccount("a@b.com", -1);
+        assertEquals(0, atmOne.checkBalance(bankAccountThree)); //Balance should be 0
+
+    }
+    @Test
+    void depositTest() throws IllegalArgumentException{
+        BankAccount bankAccount = new BankAccount("a@b.com", 200);
+        ATM atmOne = new ATM();
+        //test postive number
+        atmOne.deposit(bankAccount, 100);
+        assertEquals(300, atmOne.checkBalance(bankAccount)); 
+        //test negative number
+        assertThrows(IllegalArgumentException.class, ()->atmOne.deposit(bankAccount, 100.111));
+        assertThrows(IllegalArgumentException.class, ()->atmOne.deposit(bankAccount, -1));
+
+    }
+
+     @Test
+    void withdrawTest() throws IllegalArgumentException, InsufficientFundsException {
+        BankAccount bankAccount = new BankAccount("a@b.com", 200);
+        ATM atmOne = new ATM();
+        atmOne.withdraw(bankAccount, 100);
+        assertEquals(100, bankAccount.getBalance());
+        assertThrows(IllegalArgumentException.class, () -> atmOne.withdraw(bankAccount, -1));
+        assertThrows(IllegalArgumentException.class, ()->atmOne.withdraw(bankAccount, 100.111));
+        assertThrows(IllegalArgumentException.class, () -> atmOne.withdraw(bankAccount, 50.555));
+        assertThrows(InsufficientFundsException.class, () -> atmOne.withdraw(bankAccount, 200));
+        atmOne.withdraw(bankAccount, 0);
+        assertEquals(100, bankAccount.getBalance());
+        atmOne.withdraw(bankAccount, .50);
+        assertEquals(99.50, bankAccount.getBalance());
+    }
+}
