59f7b41a833b41cfd83fcc36c66f4d58c8cb3ee4
Cameron <carnold1@ithaca.edu>
2021-02-21 21:21:36 -0500

    Adding Admin, and a skeleton for the tests. I don't have access to the diagram currently, and wouldn't want to add more tests/methods without being accurate to the diagram

---
+++ src/test/java/edu/ithaca/dragon/bank/adminTest.java
@@ -0,0 +1,19 @@
+package edu.ithaca.dragon.bank;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class adminTest {
+    @Test
+    void freezeUnfreeze() {
+        //TODO
+        /*  Going to test freezing and unfreezing an account
+        To freeze an account requires that we add a boolean "active" attribute to the BankAccount class
+        This method will checkbalance(), then freeze the account, checkbalance() and unfreeze, checkbalance() to show
+        that the method works. This can be done with more than just the checkblanace() method.
+         */
+
+        //I couldn't complete because for some reason I don't have access to the diagram as of current, and didn't want
+        //to make a mistake
+    }
+}
