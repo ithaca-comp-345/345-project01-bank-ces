0ba45e04021c9726bbd8515960eadeab68f5d1d7
Sean Blackford <57670508+SeanBlackford37@users.noreply.github.com>
2021-02-28 14:36:50 -0500

    created central bank

---
+++ src/main/java/edu/ithaca/dragon/bank/CentralBank.java
@@ -0,0 +1,17 @@
+package edu.ithaca.dragon.bank;
+
+import java.util.ArrayList;
+
+public class CentralBank {
+    
+    private ArrayList<BankAccount> bankAccounts = new ArrayList<>();
+
+    public BankAccount findAccount (String accountID){
+        return null;
+        
+    }
+
+    public ArrayList<BankAccount> getAllAccounts (){
+        return null;
+    }
+}
